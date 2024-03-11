05b971b4db497a6d65dcc662777f760b148fd504
martin <skarbala.martin@gmail.com>
2024-03-11 14:45:14 +0100

    updated homework solution

--- tests/lecture-2/homework-solution.spec.ts
+++ tests/lecture-2/homework-solution.spec.ts
@@ -1,10 +1,13 @@
-import test, { Page, expect } from "@playwright/test";
-
+import test, { type Page, expect } from "@playwright/test";
+import offers from '../../offers.json'
 test('create a new offer', async ({ page }) => {
     await page.goto('http://localhost:8080/#/gringottsBank')
-    await page.locator('#selectedFund').selectOption('Dementor Defense Dividends')
-    await page.locator('#oneTimeInvestment').fill('10000')
-    await page.locator('#years').fill('10')
+    const offerData = {
+        fund: 'Dementor Defense Dividends',
+        investment: '10000',
+        years: '10'
+    }
+    await fillOfferData(page, offerData)
     await page.locator('[data-test="create-offer"]').click()
     const yourData = page.locator('.your-data')
     await expect(yourData.getByText('Investment')).toBeVisible()
@@ -15,21 +18,29 @@ test('create a new offer', async ({ page }) => {
 
 test('create a new investment', async ({ page }) => {
     await page.goto('http://localhost:8080/#/gringottsBank')
-    await page.locator('#selectedFund').selectOption('Dementor Defense Dividends')
-    await page.locator('#oneTimeInvestment').fill('10000')
-    await page.locator('#years').fill('10')
+    const name = 'Severus'
+    const offerData = {
+        fund: 'Dementor Defense Dividends',
+        investment: '10000',
+        years: '10'
+    }
+    await fillOfferData(page, offerData)
     await page.locator('[data-test="create-offer"]').click()
-    await page.locator('#customerName').fill('Severus')
+    await page.locator('#customerName').fill(name)
     await page.getByRole('button', { name: 'Create Investment' }).click()
     const investment = page.locator('ul.investment-list').locator('li')
     await expect(investment).toBeVisible()
-    await expect(investment.locator('h4')).toContainText('Severus')
+    await expect(investment.locator('h4')).toContainText(name)
 })
 test('reject an offer', async ({ page }) => {
     await page.goto('http://localhost:8080/#/gringottsBank')
-    await page.locator('#selectedFund').selectOption('Dementor Defense Dividends')
-    await page.locator('#oneTimeInvestment').fill('10000')
-    await page.locator('#years').fill('10')
+    const offerData = {
+        fund: 'Dementor Defense Dividends',
+        investment: '10000',
+        years: '10'
+    }
+    await fillOfferData(page, offerData)
+
     await page.locator('[data-test="create-offer"]').click()
     await page.getByRole('button', { name: 'Reject Offer' }).click()
     await expect(page.locator('.offer-detail')).not.toBeVisible()
@@ -42,17 +53,19 @@ test('reject an offer', async ({ page }) => {
 
 test('create 2 investments', async ({ page }) => {
     await page.goto('http://localhost:8080/#/gringottsBank')
-    for (let index = 0; index < 2; index++) {
-        await createInvestment(page)
+    for (const offer of offers) {
+        await fillOfferData(page, offer)
+        await page.locator('[data-test="create-offer"]').click()
+        await page.locator('#customerName').fill('Severus')
+        await page.getByRole('button', { name: 'Create Investment' }).click()
     }
-    await expect(page.locator('ul.investment-list').locator('li')).toHaveCount(2)
+    await expect(page.locator('ul.investment-list').locator('li')).toHaveCount(offers.length)
+    //show the filter
+    await expect(page.locator('ul.investment-list').locator('li').filter({ hasText: offers[0].fund })).toBeVisible()
 })
 
-async function createInvestment(page: Page) {
-    await page.locator('#selectedFund').selectOption('Dementor Defense Dividends')
-    await page.locator('#oneTimeInvestment').fill('10000')
-    await page.locator('#years').fill('10')
-    await page.locator('[data-test="create-offer"]').click()
-    await page.locator('#customerName').fill('Severus')
-    await page.getByRole('button', { name: 'Create Investment' }).click()
+async function fillOfferData(page: Page, offerData) {
+    await page.locator('#selectedFund').selectOption(offerData.fund)
+    await page.locator('#oneTimeInvestment').fill(offerData.investment)
+    await page.locator('#years').fill(offerData.years)
 }
